54e803d713cd66fac51e93a0bffeb5c8a406c5cb
blaserlab <bradley.blaser@osumc.edu>
2022-02-19 14:41:58 -0500

    made html function

--- data-raw/dev_package.R
+++ data-raw/dev_package.R
@@ -1,18 +1,26 @@
 # package setup
-library(usethis)
-create_package("/workspace/workspace_pipelines/datascience.curriculum")
-use_mit_license("Brad Blaser")
-use_readme_md()
-use_news_md()
-use_git()
-use_github(private = TRUE)
-use_vignette("week_1")
-use_vignette("week_2")
-use_vignette("week_3")
-use_vignette("week_6")
+# library(usethis)
+# create_package("/workspace/workspace_pipelines/datascience.curriculum")
+# use_mit_license("Brad Blaser")
+# use_readme_md()
+# use_news_md()
+# use_git()
+# use_github(private = TRUE)
+# use_vignette("week_1")
+# use_vignette("week_2")
+# use_vignette("week_3")
+# use_vignette("week_6")
 
 
 document()
+
+purrr::walk(.x = list.files(path = "vignettes", pattern = "*.Rmd", full.names = FALSE),
+     .f = function(x) {
+       rmarkdown::render(input = file.path("vignettes", x), output_dir = "docs", output_format = "html_document")
+       rfile <- str_replace(x, ".Rmd", ".R")
+       knitr::purl(input = file.path("vignettes", x), output = file.path("docs", rfile))
+     })
+
 # commit and push
 devtools::install_github("blaserlab/datascience.curriculum", build_vignettes = T, force = TRUE)
 build()
--- docs/week_1.R
+++ docs/week_1.R
@@ -1,7 +1,8 @@
-## ----setup, include=FALSE-----------------------------------------------------
+## ----setup, include=FALSE--------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 library(datascience.curriculum)
 
-## ---- include = FALSE---------------------------------------------------------
+
+## ---- include = FALSE------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 knitr::opts_chunk$set(
   collapse = TRUE,
   comment = "#>",
@@ -9,33 +10,51 @@ knitr::opts_chunk$set(
   message = FALSE
 )
 
-## ----eval=FALSE---------------------------------------------------------------
-#  # create a project called rclass_example in your home directory
-#  usethis::create_project("rclass_example")
-#  
-#  # generate a standard license file for your work
-#  usethis::use_mit_license("Brad Blaser")
-#  
-#  # generate a readme file so you can document your work
-#  usethis::use_readme_md()
-#  
-#  # use git and set up a private remote repository on github
-#  # these will fail unless you have git installed on your computer and
-#  # have a github account.
-#  usethis::use_git()
-#  usethis::use_github(private = TRUE)
 
-## -----------------------------------------------------------------------------
+## ----eval=FALSE------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+## # create a project called rclass_example in your home directory
+## usethis::create_project("rclass_example")
+## 
+## # generate a standard license file for your work
+## usethis::use_mit_license("Brad Blaser")
+## 
+## # generate a readme file so you can document your work
+## usethis::use_readme_md()
+## 
+## # use git and set up a private remote repository on github
+## # these will fail unless you have git installed on your computer and
+## # have a github account.
+## usethis::use_git()
+## usethis::use_github(private = TRUE)
+
+
+## --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 # produce a list of all environments
 search()
 
-## ----eval=FALSE---------------------------------------------------------------
-#  # initiate an Renv environment in a new project
-#  renv::init()
-#  
-#  # install packages listed in the lockfile
-#  renv::restore()
-#  
-#  # install any packages that get missed
-#  renv::install()
+
+## ----eval=FALSE------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+## # initiate an Renv environment in a new project
+## renv::init()
+## 
+## # install packages listed in the lockfile
+## renv::restore()
+## 
+## # install any packages that get missed
+## renv::install()
+
+
+## ----eval = FALSE----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+## # make sure you have an account and are signed in
+## 
+## # create a personal access token at the github website
+## usethis::create_github_token()
+## 
+## # run this and enter your token at the prompt
+## gitcreds::gitcreds_set()
+## 
+## # if you encounter errors you may have an old token stored elsewhere on your system.
+## # to fix this, run this command, delete the PAT line, save, restart R and try again.
+## usethis::edit_r_environ()
+## 
 
