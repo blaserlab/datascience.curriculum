7b2ee78f8541a3bea49d2594af87fe6e4d0c1253
blaserlab <bradley.blaser@osumc.edu>
2022-02-21 14:52:50 -0500

    edited section on Renv

--- data-raw/dev_package.R
+++ data-raw/dev_package.R
@@ -34,7 +34,7 @@ purrr::walk(
 )
 
 # commit and push
-gert::git_commit_all("updated readme")
+gert::git_commit_all("edited section on Renv")
 gert::git_push()
 
 devtools::install_github("blaserlab/datascience.curriculum", build_vignettes = T, force = TRUE)
