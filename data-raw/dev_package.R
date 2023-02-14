beec5d0ec0d6018a2bad827199a1035bdd9ffae1
blaserlab <bradley.blaser@osumc.edu>
2023-02-14 05:33:34 -0500

    renamed readme

--- data-raw/dev_package.R
+++ data-raw/dev_package.R
@@ -1,8 +1,5 @@
-# Run once to configure package to use pkgdown
-usethis::use_pkgdown()
 # Run to build the website
 pkgdown::build_site(install = TRUE)
-usethis::use_pkgdown_github_pages()
 
 
 devtools::document()
